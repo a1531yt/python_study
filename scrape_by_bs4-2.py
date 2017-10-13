931980395b32beb6f4ff8757c39b4466cd34229d
Yoshiyuki TAKEUCHI <a1531yt@aiit.ac.jp>
2017-10-13 08:00:58 +0000

    Scraping scripts updated

--- scrape_by_bs4-2.py
+++ scrape_by_bs4-2.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-
+#
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
 
 # HTMLファイルを読み込んでBeautifulSoupオブジェクトを得る。
-with open('./aiit.ac.jp/master_program/isa/index.html') as f:
-    soup = BeautifulSoup(f, 'html.parser')
-f.close
+with urlopen('http://aiit.ac.jp/master_program/isa/index.html') as r:
+    soup = BeautifulSoup(r, 'html.parser')
+r.close
 
 # find_all()メソッドでa要素のリストを取得して、個々のa要素に対して処理を行う。
 for a in soup.find_all('a'):
--- scrape_by_feedparser.py
+++ scrape_by_feedparser.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 #
-import feedparser
+from feedparser import parse
 
 # はてなブックマークの人気エントリー（「テクノロジー」カテゴリ）のRSSを読み込む。
 # d = feedparser.parse('http://d.hatena.ne.jp/pklib/rss')
-d = feedparser.parse('https://aiit.ac.jp/events.rss')
+d = parse('https://aiit.ac.jp/events.rss')
 
 # すべての要素について処理を繰り返す。
 for entry in d.entries:
